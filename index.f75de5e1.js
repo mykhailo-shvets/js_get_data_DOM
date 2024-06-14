163e2922f2bcc147100f1f09a22c118ef1a95d27
Mykhailo Shvets <mikhailshvets48@gmail.com>
2024-06-14 18:47:07 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const population = [
+    ...document.querySelectorAll(".population")
+];
+const totalPopulation = document.querySelector(".total-population");
+const avaragePopulation = document.querySelector(".average-population");
+const total = population.map((el)=>+el.textContent.split(",").join("")).reduce((sum, num)=>sum + num, 0);
+const avarege = total / population.length;
+totalPopulation.textContent = total.toLocaleString("en-US");
+avaragePopulation.textContent = avarege.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
